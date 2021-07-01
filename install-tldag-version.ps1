2d7848b51c525f2ed179726ef1ac88baa289d667
rhjoerg <rhjoerg@gmail.com>
2021-07-01 10:44:18 +0200

    Update install-tldag-version.ps1

--- install-tldag-version.ps1
+++ install-tldag-version.ps1
@@ -6,7 +6,6 @@ $UserHome = (Get-Item -Path "~").FullName
 $InstallDir = [System.IO.Path]::Combine($UserHome, "tldag-version")
 $ZipPath = [System.IO.Path]::Combine($InstallDir, "tldag-version.zip")
 $ExePath = [System.IO.Path]::Combine($InstallDir, "tldag-version.exe")
-$RegistryKey = "Registry::HKCU\Environment"
 
 Write-Host "Installation Directory: " $InstallDir
 
@@ -20,25 +19,4 @@ else
     New-Item -Path $InstallDir -ItemType "directory" -ErrorAction Ignore | Out-Null
     Invoke-WebRequest -Uri "https://github.com/tldag/tldag-version/releases/download/latest/tldag-version.zip" -OutFile $ZipPath
     Expand-Archive -Path $ZipPath -DestinationPath $InstallDir
-}
-
-$OldPath = (Get-ItemProperty -Path $RegistryKey -Name "Path").Path
-
-if ($OldPath.Contains($InstallDir))
-{
-    Write-Host "Path environment variable contains " $InstallDir
-}
-else
-{
-    Write-Host "Adjusting Path environment variable"
-
-    $NewPath = $OldPath
-
-    if (!$NewPath.EndsWith(";"))
-    {
-        $NewPath = $NewPath + ";"
-    }
-
-    $NewPath = $NewPath + $InstallDir
-    Set-ItemProperty -Path "$RegistryKey" -Name "Path" -Value $NewPath
 }
